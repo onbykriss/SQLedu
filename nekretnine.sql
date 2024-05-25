14c7d48a7da54385ead2045349fd1b2afebb4e56
Kristina <fashionbykriss@gmail.com>
2024-05-25 15:07:35 +0200

    kreiranje tablica

---
+++ nekretnine.sql
@@ -0,0 +1,11 @@
+--create database nekretnine;
+
+-- kreirana tablica stan
+
+-- create table stan(
+-- kvadratura int,
+-- adresa varchar(100),
+-- oprema varchar(100),
+-- slika varchar(100)
+--);
+
