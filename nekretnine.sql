238f714b225ae84b7849cb8ef09c61f9878f0ec4
Kristina <fashionbykriss@gmail.com>
2024-06-02 16:38:10 +0200

    novija verzij

--- nekretnine.sql
+++ nekretnine.sql
@@ -1,12 +1,19 @@
---create database nekretnine;
 
---create table stan(
---idstana int primary key,
---kvadratura int,
---adresa varchar(100),
---oprema varchar(100),
---slika varchar(100)
---);
+use master;
+go
+drop database if exists nekretnine;
+go
+create database nekretnine;
+go
+use nekretnine;
+
+create table stan(
+idstana int not null primary key identity(1,1),
+kvadratura int not null,
+adresa varchar(100) not null,  --ali se null ne piše nego se podrazumjeva
+oprema varchar(100),
+slika varchar(100)
+);
 
  --create table zakupac(
  --idzakupca int primary key,
