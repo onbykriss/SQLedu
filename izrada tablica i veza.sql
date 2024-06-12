9d2251fb8be2608acca4635aeb1676cc737fd418
Kristina Andric <fashionbykriss@gmail.com>
2024-06-12 18:03:59 +0200

    Update izrada tablica i veza.sql

--- izrada tablica i veza.sql
+++ izrada tablica i veza.sql
@@ -1,36 +1,36 @@
 
---use master;
---go
---drop database if exists zellamsee;
---go
---create database zellamsee;
---go
---use zellamsee;
+use master;
+go
+drop database if exists zellamsee;
+go
+create database zellamsee;
+go
+use zellamsee;
 
 
---create table stanovi(
---idstanovi int not null primary key identity(1,1),
---kvadratura int not null,
---adresa varchar(100) not null,
---oprema varchar(100),
---slika varchar(100)
---);
+create table stanovi(
+idstanovi int not null primary key identity(1,1),
+kvadratura int not null,
+adresa varchar(100) not null,
+oprema varchar(100),
+slika varchar(100)
+);
 
--- create table zakupci(
--- idzakupci int not null primary key identity(1,1),
--- ime varchar(100),
--- prezime varchar(100),
--- email varchar(100) not null,
--- telefon varchar(100)
--- );
+ create table zakupci(
+ idzakupci int not null primary key identity(1,1),
+ ime varchar(100),
+ prezime varchar(100),
+ email varchar(100) not null,
+ telefon varchar(100)
+ );
 
--- create table najmovi(
--- idstanovi int not null,
--- idzakupci int not null,
--- datumpocetka date,
--- datumzavrsetka date,
--- cijenanajma money
---);
+ create table najmovi(
+ idstanovi int not null,
+ idzakupci int not null,
+ datumpocetka date,
+ datumzavrsetka date,
+ cijenanajma money
+);
 
---alter table najmovi add foreign key (idstanovi) references stanovi(idstanovi);
---alter table najmovi add foreign key (idzakupci) references zakupci(idzakupci);
+alter table najmovi add foreign key (idstanovi) references stanovi(idstanovi);
+alter table najmovi add foreign key (idzakupci) references zakupci(idzakupci);
